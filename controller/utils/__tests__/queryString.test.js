66cd2934eef6e177049e74c7ddf512667b1622dd
gamzegulyol <ggulyol@gmail.com>
2019-12-22 15:10:25 +0300

    bootstrap added

--- controller/utils/__tests__/queryString.test.js
+++ controller/utils/__tests__/queryString.test.js
@@ -1,13 +1,15 @@
-const {parseURL,parseQueryString} = require('../queryString');
+const {parseURL, parseQueryString} = require('../queryString');
 
 describe("testing querystring", () => {
     test("url is parsed", () => {
         const exampleURL = "http://localhost:8080/?page=2&limit=3"
         expect(parseURL(exampleURL).query).toBe("page=2&limit=3");       
     })
-    test("converted to object",() =>{
-        const exampleURL= "http://localhost:8080/?page=2&limit=3"
+    test("converted to object", () => {
+        const exampleURL = "http://localhost:8080/?page=2&limit=3"
         const queryString = parseQueryString(exampleURL);
-        //expect(parseQueryString(queryString).toBe("{"page":"2","limit":"3"}));
+        console.log(queryString);
+        expect(typeof queryString).toBe("object");
+        expect(JSON.stringify(queryString)).toBe("{\"page\":\"2\",\"limit\":\"3\"}");
     })
 })
